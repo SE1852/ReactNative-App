89dd6c6db26c42c9506cb529fefe341a9bee5a17
setqt <tqthai1852@gmail.com>
2022-06-18 15:38:25 +0700

    setting 1.1

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+    render() {
+        return (
+            <View style={styles.view}>
+                
+                <Text style={styles.red}>  just red</Text>
+                <Text style={styles.bigblue}>just bigblue</Text>
+                <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+                <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    view:{
+      marginTop: 50,
+       flex:1,
+       flexDirection:'column',
+       justifyContent:'center',
+       alignItems:'center'
+    },
+    bigblue: {
+      flex:1,
+        color: 'green',
+        fontWeight: 'bold',
+        fontSize: 30,
+       // font-fam
+
+    },
+    red: {
+     
+      flex:1,
+      //fontFamily:'bigblue',
+        color: 'red',
+
+    },
 });
+
+export default App;
